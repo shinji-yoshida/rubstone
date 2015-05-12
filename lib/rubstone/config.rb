6218cae4f04aa3fa304571cf8bf0f96fef4033db
shinji-yoshida <yoshida.shinji.gm@gmail.com>
2015-05-12 17:10:53 +0900

    update

--- lib/rubstone/config.rb
+++ lib/rubstone/config.rb
@@ -6,19 +6,19 @@ module Rubstone
   class Config
     attr_reader :cache_root
     attr_reader :lib_root
-    attr_reader :dirctories
+    attr_reader :directories
     attr_reader :tagged_directory_map
 
     def initialize(hash)
       hash.assert_valid_keys("cache_root", "lib_root", "directories")
       @cache_root = hash["cache_root"]
       @lib_root = hash["lib_root"]
-      @dirctories = hash["directories"]
+      @directories = hash["directories"]
       raise "cache_root is not set" if @cache_root.blank?
-      if @lib_root.blank? && @dirctories.blank?
+      if @lib_root.blank? && @directories.blank?
         raise "lib_root or directories should be set"
       end
-      if @dirctories.present?
+      if @directories.present?
         @tagged_directory_map = Rubstone::TaggedDirectoryMap.new(@directories)
       end
     end
